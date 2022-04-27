eb6b95585354c69bcafdf90dd82b7143592b77bf
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2022-04-27 16:40:14 +0200

    Fixing `parse_decimal_date` for new formats

--- osc_builder/util.py
+++ osc_builder/util.py
@@ -1,12 +1,19 @@
 from datetime import date
-from typing import Any
+from typing import Any, Optional
 
 
-def parse_decimal_date(source):
+def parse_decimal_date(source: Optional[str]):
     if not source:
         return None
-    year, month = source.split(".")
-    return date(int(year), int(month) + 1, 1)
+    dot_count = source.count(".")
+    if not dot_count:
+        return date(int(source), 1, 1)
+    if dot_count == 1:
+        year, month = source.split(".")
+        return date(int(year), int(month) + 1, 1)
+    elif dot_count == 2:
+        year, month, day = source.split(".")
+        return date(int(year), int(month), int(day))
 
 
 def get_depth(maybe_list: Any) -> int:
